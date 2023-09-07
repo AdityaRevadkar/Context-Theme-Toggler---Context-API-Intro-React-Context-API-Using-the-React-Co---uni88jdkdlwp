33220867cb60cb8071e309a92f34a3a9b0c4dd3a
AdityaRevadkar <129678008+AdityaRevadkar@users.noreply.github.com>
2023-09-07 07:01:40 +0530

    Update LocalThemedBox.js

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -6,6 +6,30 @@ const LocalThemedBox = () => {
 return(
     <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
         {/* Write code below this line */}
+      const contextVal = useContext(ThemeContext);
+
+  const { theme } = contextVal;
+
+  const [localTheme, setLocalTheme] = useState(theme); // default 1st rended
+
+  //   Asynconous handling -> Side effect
+  useEffect(() => {
+    setLocalTheme(theme);
+  }, [theme]);
+
+  <p className={`txt-${localTheme}`} id="local-theme-text-container">
+        Hii
+      </p>
+
+      <button
+        id="local-theme-toggler"
+        className={`btn btn-${localTheme} txt-${localTheme}`}
+        onClick={() =>
+          setLocalTheme((oldTheme) => (oldTheme === "light" ? "dark" : "light"))
+        }
+      >
+        Toggle local theme to {localTheme === "light" ? "dark" : "light"}
+      </button>
         
     </div>
 )
